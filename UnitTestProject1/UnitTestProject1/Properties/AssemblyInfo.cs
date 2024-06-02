b346fca256b2fde24c01cc7db7865714ace1ca4d
holbok-peter <holbok.peter@students.jedlik.eu>
2024-06-02 13:27:39 +0200

    test_most_man_jo

---
+++ UnitTestProject1/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("88df7bf7-e8b4-414a-9538-3704b5c0a838")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1 : IDisposable
+    {
+
+        private IWebDriver driver;
+
+        public void Dispose()
+        {
+            driver.Quit();
+        }
+
+
+        public void GameTests()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://127.0.0.1:5500/index.html");
+            Thread.Sleep(1000);
+
+        }
+
+        public void PressKeyGlobally(char key)
+        {
+            Actions action = new Actions(driver);
+            action.SendKeys(key.ToString()).Perform();
+        }
+
+
+        [TestMethod]
+        public void ReachedTheGoal()
+        {
+            GameTests();
+            foreach (var charakter in "sddssssaaaaaaaawwaaaaaaassssddwwdddssddddssddddddssssssaaaaaaaaaaaaaaaaas")
+            {
+                PressKeyGlobally(charakter);
+            }
+            Assert.AreEqual("Gratulálunk, célba értél!", driver.FindElement(By.Id("popup-message")).Text);
+        }
+
+        [TestMethod]
+        public void HitTheThorn()
+        {
+            GameTests();
+            foreach (var charakter in "sddssssaaaaaawwwwa")
+            {
+                PressKeyGlobally(charakter);
+            }
+            Assert.AreEqual("Meghaltál egy tüskében!", driver.FindElement(By.Id("popup-message")).Text);
+        }
+
+
+
+    }
+}
